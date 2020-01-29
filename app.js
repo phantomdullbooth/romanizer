48e623104129c9584fc8b2464396bd6d27a8797f
Addison <49768250+winslowdev@users.noreply.github.com>
2020-01-29 00:44:55 -0500

    corrected year to account for ones digit zero

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 $(() => {
+    console.log('i like music. check out \'Up in Flames\' by Years & Years')
 
-      console.log('i like music. check out \'Up in Flames\' by Years & Years')
+    const months = ['Ianuarius', 'Februarius', 'Martius', 'Aprilis', 'Maius', 'Iunius', 'Iulius', 'Augustus', 'September', 'October', 'November', 'December']
+    const numeralOnes = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+    const numeralTens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
 
-      const months = ['Ianuarius', 'Februarius', 'Martius', 'Aprilis', 'Maius', 'Iunius', 'Iulius', 'Augustus', 'September', 'October', 'November', 'December']
-      const numeralOnes = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
-      const numeralTens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+    // EXCHANGE ARABIC DATES/YEARS FOR ROMAN NUMERALS
+    const romanizeDigits = (number) => {
+        let digits = number.toString().split('')
+        let splitDigits;
+        let romanizedDate;
 
-      // RETURN DIGITS FOR DATE AND LAST TWO OF YEAR AS ROMAN
-      const romanizeDigits = (number) => {
-            const digits = number.toString().split('');
-            let splitDigits;
-            let romanizedDate;
+        if (digits.length === 4) {
+            splitDigits = digits.splice(2, 2)
 
-            if (digits.length === 4) {
-                  splitDigits = digits.splice(2, 2)
-                  romanizedDate = numeralTens[parseInt(splitDigits[0]) - 1] + numeralOnes[parseInt(splitDigits[1]) - 1]
-            } else if (digits.length === 2) {
-                  splitDigits = digits
-                  romanizedDate = numeralTens[parseInt(splitDigits[0]) - 1] + numeralOnes[parseInt(splitDigits[1]) - 1]
-            } else if (digits.length === 1) {
-                  romanizedDate = numeralOnes[number - 1]
+            if (parseInt(splitDigits[1]) === 0) {
+                romanizedDate = numeralTens[parseInt(splitDigits[0]) - 1]
+            } else if (parseInt(splitDigits[1]) !== 0) {
+                romanizedDate = numeralTens[parseInt(splitDigits[0]) - 1] + numeralOnes[parseInt(splitDigits[1]) - 1]
             }
+        } else if (digits.length === 2) {
+            splitDigits = digits
+            romanizedDate = numeralTens[parseInt(splitDigits[0]) - 1] + numeralOnes[parseInt(splitDigits[1]) - 1]
+        } else if (digits.length === 1) {
+            romanizedDate = numeralOnes[number - 1]
+        }
+        return romanizedDate
+    }
 
-            return romanizedDate
-      }
+    // RETURN COMPLETE DATE
+    const getDate = () => {
+        const thisDate = new Date()
+        const month = thisDate.getMonth()
+        const date = thisDate.getDate()
+        const year = thisDate.getFullYear()
 
-      // RETURN COMPLETE DATE
-      const getDate = () => {
-            const thisDate = new Date()
-            const month = thisDate.getMonth()
-            const date = thisDate.getDate()
-            const year = thisDate.getFullYear()
+        return months[month] + ' ' + romanizeDigits(date) + ', MM' + romanizeDigits(year)
+    }
 
-            return months[month] + ' ' + romanizeDigits(date) + ', MM' + romanizeDigits(year)
-      }
-
-      const $romanDate = $('<h1>').text(getDate()).appendTo('main')
+    const $romanDate = $('<h1>').text(getDate()).appendTo('main')
 })
